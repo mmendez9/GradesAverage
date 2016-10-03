56176f95e6d716e291ca77494e4a3450c0bb39e1
mmendez9 <hikari1995>
2016-10-03 09:26:16 -0400

    Different version for Exam Grades

--- GradesAverage/Form1.Designer.cs
+++ GradesAverage/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Exam1 = new System.Windows.Forms.TextBox();
+            this.Exam2 = new System.Windows.Forms.TextBox();
+            this.tbxAverage = new System.Windows.Forms.TextBox();
+            this.btmCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Exam1
+            // 
+            this.Exam1.Location = new System.Drawing.Point(83, 13);
+            this.Exam1.Name = "Exam1";
+            this.Exam1.Size = new System.Drawing.Size(100, 20);
+            this.Exam1.TabIndex = 0;
+            // 
+            // Exam2
+            // 
+            this.Exam2.Location = new System.Drawing.Point(83, 53);
+            this.Exam2.Name = "Exam2";
+            this.Exam2.Size = new System.Drawing.Size(100, 20);
+            this.Exam2.TabIndex = 1;
+            // 
+            // tbxAverage
+            // 
+            this.tbxAverage.Enabled = false;
+            this.tbxAverage.Location = new System.Drawing.Point(83, 146);
+            this.tbxAverage.Name = "tbxAverage";
+            this.tbxAverage.Size = new System.Drawing.Size(100, 20);
+            this.tbxAverage.TabIndex = 2;
+            // 
+            // btmCalculate
+            // 
+            this.btmCalculate.Location = new System.Drawing.Point(96, 89);
+            this.btmCalculate.Name = "btmCalculate";
+            this.btmCalculate.Size = new System.Drawing.Size(76, 37);
+            this.btmCalculate.TabIndex = 3;
+            this.btmCalculate.Text = "Calculate Average";
+            this.btmCalculate.UseVisualStyleBackColor = true;
+            this.btmCalculate.Click += new System.EventHandler(this.btmCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Exam 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Exam 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Average";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btmCalculate);
+            this.Controls.Add(this.tbxAverage);
+            this.Controls.Add(this.Exam2);
+            this.Controls.Add(this.Exam1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Exam1;
+        private System.Windows.Forms.TextBox Exam2;
+        private System.Windows.Forms.TextBox tbxAverage;
+        private System.Windows.Forms.Button btmCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- GradesAverage/Form1.cs
+++ GradesAverage/Form1.cs
@@ -16,5 +16,50 @@ namespace GradesAverage
         {
             InitializeComponent();
         }
+        private void btmCalculate_Click(object sender, EventArgs e)
+        {
+            double exam1, exam2, average;
+            if (isInputValid(out exam1, out exam2) == true)
+            {
+                average = (exam1 + exam2) / 2;
+                tbxAverage.Text = average.ToString("f1");
+            }
+        }
+
+        private bool isInputValid(out double ex1, out double ex2)
+        {
+            ex2 = 0;
+
+            if (isFieldValid(Exam1, out ex1) && isFieldValid(Exam2, out ex2))
+                return true;
+            else
+                return false;
+        }
+
+        private bool isFieldValid(TextBox tbxName, out double grade)
+        {
+            bool isFieldOK = false;
+
+            if (double.TryParse(tbxName.Text, out grade))
+            {
+                if (grade >= 0 && grade <= 100)
+                {
+                    isFieldOK = true;
+                    return isFieldOK;
+                }
+                else
+                {
+                    MessageBox.Show(tbxName.Name.Remove(0, 3) + " out of range", "Average",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show(tbxName.Name.Remove(0, 3) + " missing or not numeric", "Average",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
     }
 }
